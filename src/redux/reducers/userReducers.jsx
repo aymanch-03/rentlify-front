7bc165d0a50a70f8da4aba090fefef050d7b8d0f
MohammedElh <Habachi20000@gmail.com>
2023-11-03 10:20:33 +0100

    Redux Added

--- src/redux/reducers/userReducers.jsx
+++ src/redux/reducers/userReducers.jsx
@@ -1,31 +1,35 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios';
 
-export const addUser = createAsyncThunk('user/addUser',async (user)=> {
+export const addUser = createAsyncThunk('user/addUser', async (user) => {
     return axios
         .post('http://localhost:5000/v1/users', user)
-        .then(console.log(res.data))
-        .catch((err)=> err);
-    }) 
+        .then(res => res.data)
+        .catch((err) => err);
+})
 
 const userSlice = createSlice({
     name: 'users',
     initialState: {
-        users :[]
+        users: []
     },
     reducers: {},
-    extraReducers:(builder)=>{
+    extraReducers: (builder) => {
         builder
-        .addCase(addUser.pending,(state,action)=>{
-            state.status="pending";
-            console.log('User Added Successfully')
-        })
-        .addCase(addUser.fulfilled,(state,action)=>{
-            state.status="fulfilled";
-        })
-        .addCase(addUser.rejected,(state,action)=>{
-            state.status="rejected";
-        })
+            .addCase(addUser.pending, (state, action) => {
+                state.status = "pending";
+                console.log('Adding New User')
+
+            })
+            .addCase(addUser.fulfilled, (state, action) => {
+                state.status = "fulfilled";
+                console.log('User Added Successfully', action.payload)
+            })
+            .addCase(addUser.rejected, (state, action) => {
+                state.status = "rejected";
+                console.log('Cannot dd Used')
+
+            })
     }
 })
 
