d10deb2630aeabeefec31afdca07ff616bc2e42c
MohammedElh <Habachi20000@gmail.com>
2023-11-06 17:31:12 +0100

    Not Finished Yet

--- src/components/Users/updateUserDialog.jsx
+++ src/components/Users/updateUserDialog.jsx
@@ -11,7 +11,7 @@ import {
 import UpdateUserForm from "./updateUserform";
 export default function UpdateUserDialog() {
   return (
-
+     
     <Dialog>
       <DialogTrigger asChild className="relative flex cursor-default select-none items-center rounded-lg px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
         <Button className='p-4' variant="outline">
