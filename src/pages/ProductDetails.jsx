1056c7c48f1aa85df7b151efcffd182bbac7c117
zakaria janah <zakariajanah3@gmail.com>
2023-12-10 11:28:06 +0000

    zack

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -63,7 +63,7 @@ export default function ListingPage() {
             </p>
 
             <form className="mt-10">
-              <Link to={`update-listing/${id}`}>
+              <Link to={`/listings/update-listing/${id}`}>
                 <button
                   type="submit"
                   className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
