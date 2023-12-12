081f102415b5282318b0387552585cab50617f0a
zakaria janah <zakariajanah3@gmail.com>
2023-12-12 13:21:51 +0000

    update-listing

--- src/pages/UpdateProducts.jsx
+++ src/pages/UpdateProducts.jsx
@@ -28,7 +28,7 @@ export default function Example() {
   const [editedImages, setEditedImages] = useState([]);
   const [category_id, setCategory_id] = useState(listing.category_id._id || "");
   const [active, setActive] = useState(listing.active );
-  const [price, setPrice] = useState(listing.price || 0);
+  const [price, setPrice] = useState(listing.price);
   const [long_description, setLong_description] = useState(
     listing.long_description || ""
   );
@@ -37,9 +37,9 @@ export default function Example() {
   );
   const [city, setCity] = useState(listing.city);
   const [province, setProvince] = useState(listing.province || "");
-  const [bed, setBed] = useState(listing.bed || 0);
-  const [room, setRoom] = useState(listing.room || 0);
-  const [max_guests, setMax_guests] = useState(listing.max_guests || 0);
+  const [bed, setBed] = useState(listing.bed);
+  const [room, setRoom] = useState(listing.room);
+  const [max_guests, setMax_guests] = useState(listing.max_guests);
 
   useEffect(() => {
     dispatch(getAllCategories());
@@ -333,7 +333,7 @@ export default function Example() {
                 />
               </div>
             </div>
-            <div className="sm:col-span-3 ">
+            <div className="sm:col-span-2 ">
               <label
                 htmlFor="room"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -350,7 +350,7 @@ export default function Example() {
                 />
               </div>
             </div>
-            <div className="sm:col-span-3 ">
+            <div className="sm:col-span-2 ">
               <label
                 htmlFor="room"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -363,6 +363,23 @@ export default function Example() {
                   name="room"
                   value={room}
                   onChange={(e) => setRoom(e.target.value)}
+                  className="p-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2 ">
+              <label
+                htmlFor="max_guests"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Max Guests
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="max_guests"
+                  value={max_guests}
+                  onChange={(e) => setMax_guests(e.target.value)}
                   className="p-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -401,23 +418,7 @@ export default function Example() {
                 />
               </div>
             </div>
-            <div className="sm:col-span-3 ">
-              <label
-                htmlFor="max_guests"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Max Guests
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="max_guests"
-                  value={max_guests}
-                  onChange={(e) => setMax_guests(e.target.value)}
-                  className="p-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+            
           </div>
           <div className="mt-6 flex items-center justify-end gap-x-6 m-10">
             <button
